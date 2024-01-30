445d3039f95b075387c129d4328d1fa5932f9f51
Ayakura Yuki <YukiAyakura@outlook.com>
2024-01-30 15:16:46 +0800

    + cases

--- examples/flow-control/src/main.rs
+++ examples/flow-control/src/main.rs
@@ -5,6 +5,10 @@ fn main() {
     println!();
     example_loop_with_index();
     println!();
+    example_match();
+    println!();
+    example_if_let_while_let();
+    println!();
 }
 
 fn example_basic_for_iter() {
@@ -39,6 +43,116 @@ fn example_loop_with_index() {
     let numbers: [i32; 10] = std::array::from_fn(|i| (i + 1) as i32);
     for (index, value) in numbers.iter().enumerate() {
         println!("第 {} 个数字是 {}", index, value);
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+enum Direction {
+    East,
+    West,
+    North,
+    South,
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub enum PokerCard {
+    // 梅花
+    Clubs(char),
+    // 黑桃
+    Spades(char),
+    // 方块
+    Diamonds(char),
+    // 红心
+    Hearts(char),
+}
+
+fn example_match() {
+    let direction = Direction::North;
+    let step = match direction {
+        // 用类似逻辑运算符【或】的写法来匹配多个模式
+        Direction::East | Direction::West => { "East or West" }
+        Direction::North => { "North" }
+        // 下划线 `_` 形似其他语言的 `default` 关键字，表示所有模式匹配中没有匹配的部分
+        _ => { "South" }
+    };
+    dbg!(step);
+
+    let coin = Coin::Penny;
+    let money = match coin {
+        Coin::Penny => {
+            // 一个模式可以做很多事，如果是需要返回结果给一个变量，最后需要用表达式结尾
+            println!("lucky penny");
+            1
+        }
+        // 一些简单的模式，可以直接返回表达式
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    };
+    dbg!(money);
+
+    let card = PokerCard::Diamonds('K');
+    // 模式绑定取出绑定的值
+    match card {
+        PokerCard::Clubs(v) => { println!("Clubs_{}", v); }
+        PokerCard::Spades(v) => { println!("Spades_{}", v); }
+        PokerCard::Diamonds(v) => { println!("Diamonds_{}", v); }
+        PokerCard::Hearts(v) => { println!("Hearts_{}", v); }
+    }
+    // 匹配没有匹配到的部分，也可以用一个变量承接其他情况
+    match card {
+        PokerCard::Hearts(v) => { println!("Hearts_{}", v); }
+        other => { println!("other cards: {:?}", other); }
+    }
+}
+
+fn example_if_let_while_let() {
+    let v = Some(3u8);
+    // 想要忽略其他的可能，只处理【3】，可以用 if let 的语法实现
+    // 类似的，对于那些只关心 Some 而不处理 None 的情况，这个语法很有用
+    // 下面这段代码等效于：
+    // match v {
+    //     Some(3) => println!("three");
+    //     _ => (),
+    // }
+    if let Some(3) = v {
+        println!("three");
+    }
+
+    // 变量遮蔽：发生在 match 或者 if let 的绑定，相当于产生了一个新的变量，这些新的变量往往不那么容易看出，所以建议使用不同的名称
+    // 为了演示，这里使用了同样的变量名来解释变量遮蔽
+    let age = Some(30);
+    println!("匹配前的 age 是 {:?}", age);
+    if let Some(age) = age {
+        println!("模式绑定的 age 是 {:?}", age);
+    }
+    println!("匹配后的 age 是 {:?}", age);
+
+    let version = Some("1.0.0");
+    println!("匹配前的 version 是 {:?}", version);
+    match version {
+        Some(version) => println!("模式绑定的 version 是 {:?}", version),
+        _ => ()
+    }
+    println!("匹配后的 version 是 {:?}", version);
+
+    // 对于需要在 Vec 做过滤的操作，有时候不能用对象和枚举直接做【==】对比，可以用 matches! 宏来比较
+    let coins = vec![Coin::Penny, Coin::Dime, Coin::Quarter];
+    let mut iter = coins.iter().filter(|c| matches!(c, Coin::Dime));
+    // while let 也是模式匹配的一种，是条件循环的一种实现，一旦匹配到 None 则停止循环
+    while let Some(coin) = iter.next() {
+        println!("filtered coin: {:?}", coin);
     }
 }
 
