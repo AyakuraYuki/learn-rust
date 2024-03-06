c0e8457664774c8506c388f9f7dd8faf0865b606
Ayakura Yuki <YukiAyakura@outlook.com>
2024-03-06 14:36:08 +0800

    + cases

--- examples/chap-4-4-5-call-and-refcall/src/main.rs
+++ examples/chap-4-4-5-call-and-refcall/src/main.rs
@@ -1,4 +1,4 @@
-use std::cell::RefCell;
+use std::cell::{Cell, RefCell};
 
 pub trait Messenger {
     fn send(&self, msg: String);
@@ -15,9 +15,32 @@ impl Messenger for MessageQueue {
     }
 }
 
+// --------------------------------------------------------------------------------
+
+fn is_even(i: i32) -> bool {
+    i % 2 == 0
+}
+
+fn retain_even(nums: &mut Vec<i32>) {
+    let slice: &[Cell<i32>] = Cell::from_mut(&mut nums[..])
+        .as_slice_of_cells();
+
+    let mut i = 0;
+    for num in slice.iter().filter(|num| is_even(num.get())) {
+        slice[i].set(num.get());
+        i += 1;
+    }
+
+    nums.truncate(i);
+}
+
+// --------------------------------------------------------------------------------
+
 fn main() {
     let mq = MessageQueue {
         message_cache: RefCell::new(Vec::new())
     };
     mq.send("hello, world".to_string());
+
+    retain_even(&mut vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 }
